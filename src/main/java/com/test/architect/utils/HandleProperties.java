54d7edc837a34e9f7a17da9eefd32e41606ca6d3
Karthikeyan Rajendran <karthis.testlab@gmail.com>
2023-02-01 00:44:37 +0530

    Added custom utils package, config.properties and BridgePatternDesign.ucls

---
+++ src/main/java/com/test/architect/utils/HandleProperties.java
@@ -0,0 +1,54 @@
+package com.test.architect.utils;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
+
+public class HandleProperties {
+
+	public static String readConfigProperties(String key) {
+		Properties properties = new Properties();
+		String value = null;
+		try {
+			FileReader reader = new FileReader("./config.properties");
+			properties.load(reader);
+			value = properties.getProperty(key);
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return value;
+	}
+
+	public static void updateConfigProperties(String key, String value) {
+		Properties properties = new Properties();
+		FileInputStream reader;
+		FileOutputStream wirter;
+		try {
+			reader = new FileInputStream("./config.properties");
+			properties.load(reader);
+			reader.close();
+			wirter = new FileOutputStream("./config.properties");
+			properties.setProperty(key, value);
+			properties.store(wirter, null);
+			wirter.close();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(readConfigProperties("Engine"));
+		System.out.println(readConfigProperties("Browser"));
+		updateConfigProperties("Engine", "Selenium");
+		System.out.println(readConfigProperties("Engine"));
+		System.out.println(readConfigProperties("Browser"));
+	}
+
+}
